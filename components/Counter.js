e701e3a8312e7b7cf7a4576d35ae08e9c462056d
Priit Kallas <kallaspriit@gmail.com>
2016-08-02 11:15:43 +0300

    Improved base logic.

--- components/Counter.js
+++ components/Counter.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { increase, decrease } from '../actions/counter';
 
-function Counter() {
+function Counter(props) {
 	return (
 		<p>
-			Hey!
+			Clicked: {props.number} times!
+			{' '}
+			<button onClick={() => props.increase(1)}>+</button>
+			{' '}
+			<button onClick={() => props.decrease(1)}>-</button>
 		</p>
 	);
 }
 
-export default Counter;
+Counter.propTypes = {
+	number: PropTypes.number.isRequired,
+	increase: PropTypes.func.isRequired,
+	decrease: PropTypes.func.isRequired,
+};
+
+export default connect(
+	state => ({ number: state.counter.number }),
+	{ increase, decrease }
+)(Counter);
