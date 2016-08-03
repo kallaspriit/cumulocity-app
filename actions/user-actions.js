e665fe69f4e92129596a09d086e4efb20ffeee99
Priit Kallas <kallaspriit@gmail.com>
2016-08-03 10:06:31 +0300

    Implemented redux promise middleware.

--- actions/user-actions.js
+++ actions/user-actions.js
@@ -1,5 +1,7 @@
-import createAsyncAction from '../util/create-async-action';
+// import createAsyncAction from '../util/create-async-action';
+import { createAction } from 'redux-actions';
 import userApi from '../apis/user-api';
 import { FETCH_USER } from '../constants';
 
-export const fetchUser = createAsyncAction(FETCH_USER, userApi.fetchUser);
+// export const fetchUser = createAsyncAction(FETCH_USER, userApi.fetchUser);
+export const fetchUser = createAction(FETCH_USER, userApi.fetchUser);
--- app.js
+++ app.js
@@ -6,9 +6,54 @@ import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import { createDevTools } from 'redux-devtools';
 import thunkMiddleware from 'redux-thunk';
-import promiseMiddleware from 'redux-promise';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+
+function promiseMiddleware() {
+	return (next) => (action) => {
+		const {
+			payload,
+			...rest,
+		} = action;
+
+		// check whether the payload looks like a promise
+		if (payload && typeof payload.then === 'function') {
+			next({
+				...rest,
+				payload: {
+					isLoading: true,
+					error: null,
+				},
+			});
+
+			payload.then(
+				(info) => {
+					next({
+						...rest,
+						payload: {
+							isLoading: false,
+							error: null,
+							info,
+						},
+					});
+				},
+				(error) => {
+					next({
+						...rest,
+						payload: {
+							isLoading: false,
+							error,
+							info: null,
+						},
+					});
+				}
+			);
+		} else {
+			// not a promise, just pass-through
+			next(action);
+		}
+	};
+}
 
 import routes from './routes';
 import * as reducers from './reducers';
