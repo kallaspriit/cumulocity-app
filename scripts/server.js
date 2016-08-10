6197d03be0371e1ea7cbeb33b544db9e913e38bb
Priit Kallas <kallaspriit@gmail.com>
2016-08-10 10:10:46 +0300

    Added support for defining server port with arguments.

--- scripts/server.js
+++ scripts/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import path from 'path';
 
-const port = 80;
+const port = process.argv.length >= 3 ? process.argv[2] : 80;
 const basePath = path.join(__dirname, '..', 'dist');
-
 const app = express();
 
 app.use('/gfx', express.static(path.join(basePath, 'gfx')));
@@ -12,6 +11,8 @@ app.use('/static', express.static(path.join(basePath, 'static')));
 app.get('*', (req, res) => {
 	res.sendFile(path.join(basePath, 'index.html'));
 });
+
+console.log(`starting server on port ${port}`);
 
 app.listen(port, () => {
 	console.log(`server running on port ${port}`);
